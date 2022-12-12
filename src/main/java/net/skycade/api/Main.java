7fc5173edaf11853fa49407603c7283a54e5968d
yakovliam <jacob@yakovliam.com>
2022-12-12 01:57:07 -0500

    feat: initial commit

---
+++ src/main/java/net/skycade/api/Main.java
@@ -0,0 +1,17 @@
+package net.skycade.api;
+
+/**
+ * Main class of the API.
+ *
+ * @author Jacob Cohen
+ */
+@SuppressWarnings("unused")
+public class Main {
+
+  /**
+   * Do something.
+   */
+  public void doSomething() {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
